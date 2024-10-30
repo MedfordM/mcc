f1a694acb1f26c432a6269e7f4924713fe8ae8d0
Mike Medford <mike@medford.dev>
2024-10-29 19:53:50 -0500

    add build script

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ -d out ]; then
+  echo '> Removing old output directory'
+  rm -rf out
+fi
+
+echo '> Creating output directory'
+mkdir out
+
+gcc -Werror -Wall -Wextra src/*.c -o out/mcc
