a90169ca668bfdbbda596791a0003c0f06cd3777
Mike Medford <mike@medford.dev>
2024-11-27 11:49:49 -0600

    fix errors

--- src/common/loadShader.cpp
+++ src/common/loadShader.cpp
@@ -1,12 +1,10 @@
 #include <string>
+#include <vector>
 #include <fstream>
 #include <sstream>
 #include "stdio.h"
 #include "stdlib.h"
 #include "GL/glew.h"
-#include "GLFW/glfw3.h"
-#include "glm/glm.hpp"
-using namespace glm;
 
 GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
 
